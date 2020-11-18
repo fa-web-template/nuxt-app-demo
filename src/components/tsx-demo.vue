35e38d0b05c187ada7f627aa7e00a3a3b1e6e739
gd4ark <gd.4ark@gmail.com>
2020-11-18 09:42:45 +0800

    feat: add vue-tsx-support

---
+++ src/components/tsx-demo.vue
@@ -0,0 +1,39 @@
+<template> </template>
+
+<script lang="tsx">
+import * as tsx from 'vue-tsx-support'
+
+export default tsx.componentFactory.create({
+  // It's not working.
+  data() {
+    return {
+      a: 1,
+    }
+  },
+  // It's working.
+  // data: () => ({
+  //   a: 1,
+  // }),
+  props: {
+    text: {type: String, required: true},
+    important: Boolean,
+  } as const, // `as const` is needed in some cases.
+  computed: {
+    className(): string {
+      return this.important ? 'label-important' : 'label-normal'
+    },
+  },
+  methods: {
+    onClick(event) {
+      this.$emit('ok', event)
+    },
+  },
+  render(): VNode {
+    return (
+      <span class={this.className} onClick={this.onClick}>
+        {this.text}
+      </span>
+    )
+  },
+})
+</script>
